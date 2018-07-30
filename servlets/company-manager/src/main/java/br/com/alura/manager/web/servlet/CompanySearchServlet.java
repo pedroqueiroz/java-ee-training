ae5e53f56206d0fca4adbf3258830b84cafe882a
Pedro Queiroz <pedro.queiroz@liferay.com>
2018-07-29 23:05:16 -0300

    Add Servlet to list Stored Companies

---
+++ servlets/company-manager/src/main/java/br/com/alura/manager/web/servlet/CompanySearchServlet.java
@@ -0,0 +1,41 @@
+package br.com.alura.manager.web.servlet;
+
+import br.com.alura.manager.Company;
+import br.com.alura.manager.dao.CompanyDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+@WebServlet(urlPatterns = "/search")
+public class CompanySearchServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("Search Result: <br/>");
+
+        String filter = req.getParameter("filter");
+
+        CompanyDAO companyDAO = new CompanyDAO();
+
+        writer.println("<ul>");
+
+        for (Company company : companyDAO.searchBySimilarity(filter)) {
+            writer.println("<li>" + company.getId() + ": " + company.getName() + "</li>");
+        }
+
+        writer.println("</ul>");
+
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
