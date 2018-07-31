21c88a0a2349d86b6898e64212926cc635ab3882
Pedro Queiroz <pedro.queiroz@liferay.com>
2018-07-31 00:28:08 -0300

    Use cookie to print user on server audit filter

--- servlets/company-manager/src/main/java/br/com/alura/manager/web/filter/AuditURLFilter.java
+++ servlets/company-manager/src/main/java/br/com/alura/manager/web/filter/AuditURLFilter.java
@@ -2,8 +2,10 @@ package br.com.alura.manager.web.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Objects;
 
 @WebFilter(urlPatterns = "/*")
 public class AuditURLFilter implements Filter {
@@ -12,8 +14,31 @@ public class AuditURLFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
 
-        System.out.println("User accessing URI " + httpServletRequest.getRequestURI());
+        Cookie cookie = getUser(httpServletRequest);
+        String user = "<unauthenticated>";
+
+        if (cookie != null) {
+            user = cookie.getValue();
+        }
+
+        System.out.println("User " + user + " accessing URI " + httpServletRequest.getRequestURI());
 
         filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private Cookie getUser(HttpServletRequest httpServletRequest) {
+        Cookie[] cookies = httpServletRequest.getCookies();
+
+        if (cookies == null) {
+            return null;
+        }
+
+        for (Cookie cookie: cookies) {
+            if (Objects.equals(cookie.getName(), "user.logged")) {
+                return cookie;
+            }
+        }
+
+        return null;
     }
 }
